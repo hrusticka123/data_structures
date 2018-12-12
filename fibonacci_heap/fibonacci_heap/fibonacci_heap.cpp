299ea18692bd59c9bc4ff706d3501c8285c7db64
hruma02 <Marcel.Hruska@ca.com>
2018-12-12 11:08:30 +0100

    init

---
+++ fibonacci_heap/fibonacci_heap/fibonacci_heap.cpp
@@ -0,0 +1,12 @@
+﻿// fibonacci_heap.cpp : Defines the entry point for the application.
+//
+
+#include "fibonacci_heap.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
